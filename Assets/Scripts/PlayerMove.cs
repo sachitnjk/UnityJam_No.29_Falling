0071e5ded4958555f83db0c459bf76890fe77b31
Sachit Nanajkar <sachitnjk@gmail.com>
2024-03-03 23:57:11 +0000

    Cannon rotation functionality, working

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,6 +13,13 @@ public class PlayerMove : MonoBehaviour
 	private float moveX;
 	private float newXPosition;
 	private float moveAmount;
+	private Vector2 rotateInput;
+	private float rotationX;
+	private float rotationZ;
+	private float targetXRotation;
+	private float targetZRotation;
+	private float rotationAmountX;
+	private float rotationAmountZ;
 
 	[Header("References required")]
 	[SerializeField] private GameObject cannonBarrel;
@@ -20,10 +27,14 @@ public class PlayerMove : MonoBehaviour
 	[Header("Cannon attribute values")]
 	[Range(5f, 20f)]
 	[SerializeField] private float moveSpeed;
-	[Range(1f, 20f)]
+	[Range(1f, 100f)]
 	[SerializeField] private float rotationSpeed;
 	[Range(10f, 40f)]
 	[SerializeField] private float xMovementClamp;
+	[Range(10f, 90f)]
+	[SerializeField] private float xRotationClamp;
+	[Range(10f, 90f)]
+	[SerializeField] private float zRotationClamp;
 
 	private void Start()
 	{
@@ -38,7 +49,7 @@ public class PlayerMove : MonoBehaviour
 	private void Update()
 	{
 		CheckCannonMove();
-		//CheckCannonRotate();
+		CheckCannonRotate();
 	}
 
 	#region CannonMove
@@ -57,8 +68,6 @@ public class PlayerMove : MonoBehaviour
 		moveX = moveInput.x;
 		moveAmount = moveX * moveSpeed * Time.deltaTime;
 
-		//this.gameObject.transform.Translate(Vector3.right * moveAmount);
-
 		newXPosition = Mathf.Clamp(transform.position.x + moveAmount, -xMovementClamp, xMovementClamp);
 		transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
 	}
@@ -76,7 +85,27 @@ public class PlayerMove : MonoBehaviour
 	}
 	private void RotateCannon()
 	{
+		rotateInput = cannonRotate.ReadValue<Vector2>();
 
+		rotationX = rotateInput.y;
+		rotationZ = rotateInput.x;
+
+		//Inverting
+		rotationAmountX = rotationX * rotationSpeed *Time.deltaTime;
+		rotationAmountZ = -rotationZ * rotationSpeed * Time.deltaTime;
+
+		//!!NOTE!! ->Mathf.Repeat is nothing but getting the modulo so Mathf.Repeat(5, 3) = 2 because 5 divided by 3 gives a remainder of 2<- !!NOTE!!
+		// !!NOTE!! unity bounds euler angles between 0 to 360 only, does not allow to go into negatives !!NOTE!!
+
+		//dding 180, repeating, and then subtracting 180, the result is a value that ranges from -180 to 180, allowing for negative rotations 
+		float currentXRotation = Mathf.Repeat(cannonBarrel.transform.eulerAngles.x + 180f, 360f) - 180f;
+		float currentZRotation = Mathf.Repeat(cannonBarrel.transform.eulerAngles.z + 180f, 360f) - 180f;
+
+		targetXRotation = Mathf.Clamp(currentXRotation + rotationAmountX, -xRotationClamp, xRotationClamp);
+		targetZRotation = Mathf.Clamp(currentZRotation + rotationAmountZ, -zRotationClamp, zRotationClamp);
+
+		Quaternion targetRotation = Quaternion.Euler(targetXRotation, cannonBarrel.transform.eulerAngles.y, targetZRotation);
+		cannonBarrel.transform.rotation = Quaternion.Slerp(cannonBarrel.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 	}
 
 	#endregion
